c8c34b9d2ea8f29141eb73b1943fa7388c6e873f
Akhystophane <emnl.busi@outlook.fr>
2024-07-25 14:55:49 -0400

    Changed loader

--- src/components/ScreenLoader.jsx
+++ src/components/ScreenLoader.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import circle from '../assets/svg/spinning-circles.svg';
+import CircularProgressWithLabel from './design/CircularProgressWithLabel'
+
 
 const ScreenLoader = ({ progress }) => {
   if (progress === 100) return null;
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black z-50">
-      <img src={circle} alt="Loading" className="w-16 h-16" />
+      {/* <img src={circle} alt="Loading" className="w-16 h-16" /> */}
+      <CircularProgressWithLabel progress={progress}/>
     </div>
   );
 };
